3ce4def27e03defb188806bb8c50135a66238021
Maksim <nersus1337@mail.ru>
2025-01-07 13:51:52 +0700

    fix

--- src/main/java/ru/accept_applicant_documents/system/repository/DepartmentRepo.java
+++ src/main/java/ru/accept_applicant_documents/system/repository/DepartmentRepo.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import ru.accept_applicant_documents.system.model.Department;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DepartmentRepo extends JpaRepository<Department, Long> {
-    //List<Department> findAll();
+
+    Department findByTitle(String title);
 }
